cea3cb6dc86ef67b7a72d10b20894c03f4cbc526
erik-andreasson-nti-johanneberg <74763563+erik-andreasson-nti-johanneberg@users.noreply.github.com>
2022-03-21 10:44:39 +0100

    model.rb complete
    #done

--- app.rb
+++ app.rb
@@ -11,13 +11,9 @@ enable :sessions
 
 #Homepage
 get('/') do
-    db = SQLite3::Database.new("db/goodgames.db")
-    db.results_as_hash = true
-    action_games = db.execute("SELECT * FROM game WHERE id IN (SELECT game_id FROM genre_relation WHERE genre_id = 1)")
-    racing_games = db.execute("SELECT * FROM game WHERE id IN (SELECT game_id FROM genre_relation WHERE genre_id = 24)")
-    p action_games
-    p racing_games
-    slim(:index, locals:{action_games:action_games, racing_games:racing_games})
+    db = connect_to_db_with_hash()
+    home_page_games = home_page_games(db)
+    slim(:index, locals:{action_games:home_page_games[0], racing_games:home_page_games[1]})
 end
 
 #login page
@@ -37,60 +33,48 @@ end
 
 #Review page
 get('/games/review') do
-    db = SQLite3::Database.new("db/goodgames.db")
-    db.results_as_hash = true
+    db = connect_to_db_with_hash()
     game_name = params[:game_name]
-    p game_name
-    game_info = db.execute("SELECT * FROM game WHERE name = ?",game_name).first
+    game_info = all_game_info(db, game_name)
+    p game_info
     slim(:"games/review", locals:{game_info:game_info})
 end
 
 #add game page
 get('/games/new') do
-    db = SQLite3::Database.new("db/goodgames.db")
-    db.results_as_hash = true
-    publisher_names = db.execute("SELECT name FROM publisher")
-    genres = db.execute("SELECT name FROM genre")
-    admin = db.execute("SELECT admin FROM user WHERE id = ?", session[:id]).first
-    games = db.execute("SELECT name FROM game")
-    slim(:"games/new", locals:{names:publisher_names, genres:genres, admin:admin, games:games})
+    db = connect_to_db_with_hash()
+    add_game = add_game(db)
+    slim(:"games/new", locals:{names:add_game[0], genres:add_game[1], admin:add_game[2], games:add_game[3]})
 end
 
 #add publisher page
 get('/publishers/new') do
-    db = SQLite3::Database.new("db/goodgames.db")
-    db.results_as_hash = true
-    admin = db.execute("SELECT admin FROM user WHERE id = ?", session[:id]).first
-    publishers = db.execute("SELECT name FROM publisher")
-    slim(:"publishers/new", locals:{admin:admin, publishers:publishers})
+    db = connect_to_db_with_hash()
+    add_publisher = add_publisher(db)
+    slim(:"publishers/new", locals:{admin:add_publisher[0], publishers:add_publisher[1]})
 end
 
 #game page when taken from button
 get('/games/index') do
-    db = SQLite3::Database.new("db/goodgames.db")
-    db.results_as_hash = true
+    db = connect_to_db_with_hash()
     game_name = params[:game_name]
-    game_id = db.execute("SELECT id FROM game WHERE name=?", game_name).first
-    game_id = game_id["id"]
-    publisher_name = db.execute("SELECT name FROM publisher WHERE id IN (SELECT publisher_id FROM game WHERE name = ?)",game_name).first
-    game_info = db.execute("SELECT * FROM game WHERE name = ?",game_name).first
-    reviews = db.execute("SELECT * FROM review WHERE game_id = ?", game_id)
+    game_page = game_page(db, game_name)
+    p game_page
+    ratings = game_page[3]
     total_rating = 0.0
-    ratings = db.execute("SELECT rating FROM review")
-    p ratings
-    ratings.each do |rating|
-        p rating
-        rating = rating["rating"]
-        rating = rating.to_f
-        total_rating += rating
+    if ratings[0] == nil
+        ra = 0
+    else
+        ratings.each do |rating|
+            rating = rating["rating"]
+            rating = rating.to_f
+            total_rating += rating
+        end
+        rating_average = total_rating/(ratings.length())
+        rating_average = rating_average.round(0).to_s
+        ra = rating_average
     end
-    p ratings.length()
-    rating_average = total_rating/(ratings.length())
-    rating_average = rating_average.round(0).to_s
-    p rating_average
-    ra = rating_average
-    p ra
-    slim(:"games/index", locals:{publisher_name:publisher_name, game_info:game_info, reviews:reviews, ra:ra})
+    slim(:"games/index", locals:{publisher_name:game_page[0], game_info:game_page[1], reviews:game_page[2], ra:ra})
 end
 
 #error page
@@ -105,34 +89,32 @@ end
 
 #profile_page 
 get('/users/index') do
-    db = SQLite3::Database.new("db/goodgames.db")
-    db.results_as_hash = true
-    profile_info = db.execute("SELECT * FROM user WHERE username = ?", session[:username]).first
-    reviews = db.execute("SELECT * FROM review WHERE username = ?", session[:username])
-    slim(:"users/index", locals:{profile_info:profile_info, reviews:reviews})
+    db = connect_to_db_with_hash()
+    profile_page = profile_page(db)
+    slim(:"users/index", locals:{profile_info:profile_page[0], reviews:profile_page[1]})
 end
 
 # get('/create_pw') do
 #     slim(:create_admin_pw)
 # end
 
+# show publishers
+get('/publishers/index') do
+    db = connect_to_db_with_hash()
+    show_publishers = show_publishers(db)
+    slim(:"publishers/index", locals:{publisher_info:show_publishers})
+end
+
 #add review
 post('/games/review') do
-    db = SQLite3::Database.new("db/goodgames.db")
-    db.execute("UPDATE user SET games_reviewed = games_reviewed + 1 WHERE username = ?", session[:username])
+    db = connect_to_db_no_hash()
     game_name=params[:game_name]
-    if params[:finished_yes]
-        finished = 1
-        db.execute("UPDATE user SET games_played = games_played + 1 WHERE username = ?", session[:username])
-    else
-        finished = 0
-    end
+    finished_q = params[:finished_yes]
     rating = params[:rating]
     hrs_played = params[:hrs_played]
     review = params[:review_text]
     review_date = Date.today.to_s
-    game_id = db.execute("SELECT id FROM game WHERE name = ?", game_name)
-    db.execute("INSERT INTO review (rating, username, comments, review_date, game_id, finished) VALUES (?,?,?,?,?,?)",rating, session[:username], review, review_date, game_id, finished)
+    add_review(db, finished_q, game_name, rating, review, review_date)
     redirect('/')
 end
 
@@ -151,8 +133,7 @@ end
 
 #account creation
 post('/users/new') do
-    db = SQLite3::Database.new("db/goodgames.db")
-    db.results_as_hash = true
+    db = connect_to_db_with_hash()
     username = params[:username]
     password = params[:password]
     password_confirm = params[:password_confirm]
@@ -160,57 +141,31 @@ post('/users/new') do
     email = params[:email]
     admin_passkey = params[:admin_passkey]
     date = Date.today.to_s
-    result = db.execute("SELECT password FROM admin_password").first
-    pw_digest = result["password"]
-
-    if password == password_confirm
-        if BCrypt::Password.new(pw_digest) == admin_passkey
-            encrypted_password = BCrypt::Password.create(password)
-            db.execute("INSERT INTO user (username, email, age, account_age, games_played, games_reviewed, password, admin) VALUES (?,?,?,?,?,?,?,?)", username, email, age, date, 0, 0, encrypted_password, 1)
-            redirect('/')
-        else
-            encrypted_password = BCrypt::Password.create(password)
-            db.execute("INSERT INTO user (username, email, age, account_age, games_played, games_reviewed, password, admin) VALUES (?,?,?,?,?,?,?,?)", username, email, age, date, 0, 0, encrypted_password, 0)
-            redirect('/')
-        end
-    else
-      redirect('/error')
-    end
+    account_creation(db, password, password_confirm, username, email, age, date, admin_passkey)
 end
 
 #account login
 post('/users/login') do
-    db = SQLite3::Database.new("db/goodgames.db")
-    db.results_as_hash = true
+    db = connect_to_db_with_hash()
     username = params[:username]
     password = params[:password]
-    result = db.execute("SELECT * FROM user WHERE username = ?", username).first
-    pwdigest = result["password"]
-    id = result["id"]
-    if BCrypt::Password.new(pwdigest) == password
-        session[:id] = id
-        session[:username] = username
-        p session[:username]
-        redirect('/')
-    else
-        redirect('/error')
-    end
+    account_login(db, username, password)
 end
 
+# add publisher post
 post('/publishers/new') do
-    db = SQLite3::Database.new("db/goodgames.db")
-    db.results_as_hash = true
+    db = connect_to_db_with_hash()
     publisher_name = params[:publisher_name]
     publisher_address = params[:publisher_address]
     publisher_phone_number = params[:publisher_phone_number]
     active_since = params[:active_since]
-    db.execute("INSERT INTO publisher (name, adress, phone_number, active_since) VALUES (?,?,?,?)",publisher_name,publisher_address,publisher_phone_number,active_since)
+    add_publisher_post(db, publisher_name, publisher_address, publisher_phone_number, active_since)
     redirect('/')
 end
 
-post('/games') do
-    db = SQLite3::Database.new("db/goodgames.db")
-    db.results_as_hash = true
+# add game post
+post('/games/new') do
+    db = connect_to_db_with_hash()
     #image form
     # Check if user uploaded a file
     filename = params[:image][:filename]
@@ -221,30 +176,14 @@ post('/games') do
         f.write(file.read)
     end
     #params
+    genre1 = params[:genre1]
+    genre2 = params[:genre2]
+    genre3 = params[:genre3]
     game_name = params[:game_name]
     release_date = params[:release_date]
     hrs_to_complete = params[:hrs_to_complete].to_i
     publisher = params[:publisher]
-    publisher_id = db.execute("SELECT id FROM publisher WHERE name = ?",publisher).first["id"].to_i
-    #game db insertion
-    if db.execute("SELECT COUNT(1) FROM game WHERE name = ?", game_name).first["COUNT(1)"] < 1
-        db.execute("INSERT INTO game (publisher_id, name,release_date, hrs_to_complete, image_file_name) VALUES (?,?,?,?,?)",publisher_id,game_name,release_date,hrs_to_complete,path)
-    else 
-        redirect('/error')
-    end
-    last_id = db.last_insert_row_id()
-    p last_id
-    #genre relation insertion
-    genre1 = db.execute("SELECT id FROM genre WHERE name = ?",params[:genre1]).first["id"].to_i
-    db.execute("INSERT INTO genre_relation (game_id, genre_id) VALUES (?,?)",last_id,genre1)
-    if params[:genre2] != nil
-        genre2 = db.execute("SELECT id FROM genre WHERE name = ?",params[:genre2]).first["id"].to_i
-        db.execute("INSERT INTO genre_relation (game_id, genre_id) VALUES (?,?)",last_id,genre2)
-    end
-    if params[:genre3] != nil
-        genre3 = db.execute("SELECT id FROM genre WHERE name = ?",params[:genre3]).first["id"].to_i
-        db.execute("INSERT INTO genre_relation (game_id, genre_id) VALUES (?,?)",last_id,genre3)
-    end
+    add_game_post(db, publisher, id, game_name, release_date, hrs_to_complete, path, genre1, genre2, genre3)
     redirect('/')
 end
 
@@ -262,13 +201,15 @@ post("/users/logout") do
 end
 
 post('/games/delete') do
-    db = SQLite3::Database.new("db/goodgames.db")
-    db.execute("DELETE FROM game WHERE name = ?", params[:game])
+    game_name = params[:game]
+    db = connect_to_db_no_hash()
+    delete_game()
     redirect('/')
 end
 
 post('/publishers/delete') do
-    db = SQLite3::Database.new("db/goodgames.db")
-    db.execute("DELETE FROM publisher WHERE name = ?", params[:publisher])
+    publisher_name = params[:publisher]
+    db = connect_to_db_no_hash()
+    delete_publisher()
     redirect('/')
 end
--- model.rb
+++ model.rb
@@ -1,1 +1,138 @@
-def connect_to_db
+def connect_to_db_with_hash()
+    db = SQLite3::Database.new("db/goodgames.db")
+    db.results_as_hash = true
+    return db
+end
+
+def connect_to_db_no_hash()
+    db = SQLite3::Database.new("db/goodgames.db")
+    return db
+end
+
+def home_page_games(db)
+    action_games = db.execute("SELECT * FROM game WHERE id IN (SELECT game_id FROM genre_relation WHERE genre_id = 1)")
+    racing_games = db.execute("SELECT * FROM game WHERE id IN (SELECT game_id FROM genre_relation WHERE genre_id = 24)")
+    return [action_games, racing_games]
+end
+
+def all_game_info(db, game_name)
+    return db.execute("SELECT * FROM game WHERE name = ?",game_name).first
+end
+
+def add_game(db)
+    publisher_names = db.execute("SELECT name FROM publisher")
+    genres = db.execute("SELECT name FROM genre")
+    admin = db.execute("SELECT admin FROM user WHERE id = ?", session[:id]).first
+    games = db.execute("SELECT name FROM game")
+    return [publisher_names, genres, admin, games]
+end
+
+def add_publisher(db)
+    admin = db.execute("SELECT admin FROM user WHERE id = ?", session[:id]).first
+    publishers = db.execute("SELECT name FROM publisher")
+    return [admin, publishers]
+end
+
+def game_page(db, game_name)
+    game_id = db.execute("SELECT id FROM game WHERE name=?", game_name).first
+    game_id = game_id["id"]
+    publisher_name = db.execute("SELECT name FROM publisher WHERE id IN (SELECT publisher_id FROM game WHERE name = ?)",game_name).first
+    if publisher_name != db.execute("SELECT publisher_name FROM game WHERE publisher_name = ?", publisher_name["name"])
+        publisher_name = db.execute("SELECT name FROM publisher WHERE name IN (SELECT publisher_name FROM game WHERE id = ?)", game_id).first
+        new_id = db.execute("SELECT id FROM publisher WHERE name = ?", publisher_name["name"]).first
+        db.execute("UPDATE game SET publisher_id = ? WHERE publisher_name = ?",new_id['id'], publisher_name["name"])
+        publisher_name = db.execute("SELECT name FROM publisher WHERE id IN (SELECT publisher_id FROM game WHERE name = ?)",game_name).first
+    end
+    game_info = db.execute("SELECT * FROM game WHERE name = ?",game_name).first
+    reviews = db.execute("SELECT * FROM review WHERE game_id = ?", game_id)
+    ratings = db.execute("SELECT rating FROM review WHERE game_id = ?", game_id)
+    return [publisher_name, game_info, reviews, ratings]
+end
+
+def show_publishers(db)
+    return db.execute("SELECT * FROM publisher")
+end
+
+def profile_page(db)
+    profile_info = db.execute("SELECT * FROM user WHERE username = ?", session[:username]).first
+    reviews = db.execute("SELECT * FROM review WHERE username = ?", session[:username])
+    return [profile_info, reviews]
+end
+
+def add_review(db, finished_q, game_name, rating, review, review_date)
+    db.execute("UPDATE user SET games_reviewed = games_reviewed + 1 WHERE username = ?", session[:username])
+    if finished_q
+        finished = 1
+        db.execute("UPDATE user SET games_played = games_played + 1 WHERE username = ?", session[:username])
+    else
+        finished = 0
+    end
+    game_id = db.execute("SELECT id FROM game WHERE name = ?", game_name)
+    db.execute("INSERT INTO review (rating, username, comments, review_date, game_id, finished) VALUES (?,?,?,?,?,?)",rating, session[:username], review, review_date, game_id, finished)
+end
+
+def account_creation(db, password, password_confirm, username, email, age, date, admin_passkey)
+    result = db.execute("SELECT password FROM admin_password").first
+    pw_digest = result["password"]
+    if password == password_confirm
+        if BCrypt::Password.new(pw_digest) == admin_passkey
+            encrypted_password = BCrypt::Password.create(password)
+            db.execute("INSERT INTO user (username, email, age, account_age, games_played, games_reviewed, password, admin) VALUES (?,?,?,?,?,?,?,?)", username, email, age, date, 0, 0, encrypted_password, 1)
+            redirect('/')
+        else
+            encrypted_password = BCrypt::Password.create(password)
+            db.execute("INSERT INTO user (username, email, age, account_age, games_played, games_reviewed, password, admin) VALUES (?,?,?,?,?,?,?,?)", username, email, age, date, 0, 0, encrypted_password, 0)
+            redirect('/')
+        end
+    else
+        redirect('/error')
+    end
+end
+
+def account_login(db, username, password)
+    result = db.execute("SELECT * FROM user WHERE username = ?", username).first
+    pwdigest = result["password"]
+    id = result["id"]
+    if BCrypt::Password.new(pwdigest) == password
+        session[:id] = id
+        session[:username] = username
+        redirect('/')
+    else
+        redirect('/error')
+    end
+end
+
+def add_publisher_post(db, publisher_name, publisher_address, publisher_phone_number, active_since)
+    db.execute("INSERT INTO publisher (name, adress, phone_number, active_since) VALUES (?,?,?,?)",publisher_name,publisher_address,publisher_phone_number,active_since)
+end
+
+def add_game_post(db, publisher, id, game_name, release_date, hrs_to_complete, path, genre1, genre2, genre3)
+    publisher_id = db.execute("SELECT id FROM publisher WHERE name = ?",publisher).first["id"].to_i
+    if db.execute("SELECT COUNT(1) FROM game WHERE name = ?", game_name).first["COUNT(1)"] < 1
+        db.execute("INSERT INTO game (publisher_id, name,release_date, hrs_to_complete, image_file_name, publisher_name) VALUES (?,?,?,?,?,?)",publisher_id,game_name,release_date,hrs_to_complete,path, publisher)
+    else 
+        redirect('/error')
+    end
+    last_id = db.last_insert_row_id()
+    #genre relation insertion
+    genre1 = db.execute("SELECT id FROM genre WHERE name = ?",genre1).first["id"].to_i
+    db.execute("INSERT INTO genre_relation (game_id, genre_id) VALUES (?,?)",last_id,genre1)
+    if params[:genre2] != nil
+        genre2 = db.execute("SELECT id FROM genre WHERE name = ?",genre2).first["id"].to_i
+        db.execute("INSERT INTO genre_relation (game_id, genre_id) VALUES (?,?)",last_id,genre2)
+    end
+    if params[:genre3] != nil
+        genre3 = db.execute("SELECT id FROM genre WHERE name = ?",genre3).first["id"].to_i
+        db.execute("INSERT INTO genre_relation (game_id, genre_id) VALUES (?,?)",last_id,genre3)
+    end
+end
+
+def delete_game(db, game_name)
+    game_id = db.execute("SELECT id FROM game WHERE name = ?", game_name)
+    db.execute("DELETE FROM genre_relation WHERE game_id = ?", game_id)
+    db.execute("DELETE FROM game WHERE name = ?", game_name)
+end
+
+def delete_publisher(db, publisher_name)
+    db.execute("DELETE FROM publisher WHERE name = ?", publisher_name)
+end
